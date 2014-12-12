fae428e6591cff0309651cb971fce890964bb521
Paolo Ardoino <paolo.ardoino@gmail.com>
2014-12-12 09:24:10 +0100

    Semplified caching and bug fix

--- examples/perf/perf.js
+++ examples/perf/perf.js
@@ -5,7 +5,7 @@ var _ = require('lodash');
 
 var chunk = 'foo';
 var wnum = +(process.argv[2] || 1);
-var probes = +(process.argv[3] || 10000);
+var probes = +(process.argv[3] || 40000);
 
 if (cluster.isMaster) {
   for (var i = 0; i < 3; i++) {
@@ -31,7 +31,7 @@ if (cluster.isMaster) {
       (function(i) {
         var worker = new pigato.Worker('tcp://127.0.0.1:55559', 'echo');
         worker.on('request', function(inp, res) {
-          //res.opts.cache = 20000;
+          res.opts.cache = 20000;
           res.end(inp + 'FINAL');
           //console.log("WORKER " + i);
         });
@@ -48,13 +48,7 @@ if (cluster.isMaster) {
     var timer;
     var rcnt = 0;
 
-    function acc() {
-      rcnt++;
-
-      if (rcnt < probes) {
-        return;
-      }
-
+    function done() {
       var elapsed = process.hrtime(timer);
       var dts = elapsed[0] + (elapsed[1] / 1000000000);
       console.log("CLIENT GOT answer", dts + " milliseconds. " + (probes / dts).toFixed(2) + " requests/sec.");
@@ -70,7 +64,13 @@ if (cluster.isMaster) {
         )
         .on('data', function() {})
         .on('end', function() {
-          acc();
+          rcnt++;
+
+          if (rcnt < probes) {
+            return;
+          }
+
+          done();
         });
       }
     }
