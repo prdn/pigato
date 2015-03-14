4ea51182602ed82159701e537bf4898c24f6ae72
Paolo Ardoino <paolo.ardoino@gmail.com>
2015-03-14 19:54:53 +0100

    Minor changes

---
+++ examples/echo/client-flood.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+var Client = require('./../../index').Client;
+
+var conf = JSON.parse(fs.readFileSync(__dirname + '/../config.json', 'UTF-8'));
+
+var client = new Client('tcp://' + conf.broker.host + ':' + conf.broker.port);
+client.start();
+
+client.on('error', function(e) {
+  console.log('ERROR', e);
+});
+
+var d1 = new Date();
+var reqs = 1000;
+
+var rcnt = 0;
+
+for (var i = 0; i < reqs; i++) {
+  client.request(
+    'echo', 'foo', 
+    function(err, data) {},
+    function(err, data) {
+      rcnt++;
+      if (rcnt === reqs) {
+        console.log(reqs + ' requests/replies processed (' + ((new Date()).getTime() - d1.getTime()) + ' milliseconds)');
+        process.exit(0);
+      }
+    }, { timeout: 10000 }
+  );
+}
