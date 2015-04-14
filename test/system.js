941416094567a344bc19a4bb37d68631348edf39
Maxence Dalmais <maxence.dalmais@worldline.com>
2015-04-14 16:46:52 +0200

    refactor test/system to make it more readable

--- test/system.js
+++ test/system.js
@@ -3,6 +3,8 @@ var zmq = require('zmq');
 var chai = require('chai');
 var uuid = require('node-uuid');
 
+var assert = chai.assert;
+
 var location = 'inproc://#';
 
 describe('FILE DESCRIPTORS', function() {
@@ -15,31 +17,68 @@ describe('FILE DESCRIPTORS', function() {
     broker.stop(done);
   });
 
-  it('Client flood', function(done) {
-    this.timeout(5 * 1000);
-
-    var cnt = 0;
-
-    function spawn(hm) { 
+  var spawn = function(hm, callback) {
+    var clients = [];
+    try {
       for (var ci = 0; ci < hm; ci++) {
         var c = new PIGATO.Client(bhost);
         c.start();
-        c.stop();
-        cnt++
+        clients.push(c);
       }
-      step();
+    } catch (err) {
+      return callback(err, clients);
     }
 
-    function step() {
-      setImmediate(function() {
-        if (cnt > 5000) {
-          done();
-          return;
-        }
-        spawn(zmq.Context.getMaxSockets() - 100);
+    callback(null, clients);
+  };
+
+  describe("When I create too many sockets", function() {
+    it('return a \'Too many open files\' error', function(done) {
+      spawn(zmq.Context.getMaxSockets() * 2, function(err, clients) {
+
+        assert.ok(err);
+        assert.ok(err.message);
+        assert.equal('Too many open files', err.message);
+        clients.forEach(function(client) {
+          client.stop();
+        });
+
+        setTimeout(done, 50);
       });
-    }
+    });
+  });
 
-    step();
+
+  describe("When I create lots of sockets", function() {
+
+    before(function(done) {
+      //lets wait a bit
+      setTimeout(done, 50);
+    });
+
+    it("still works if I close them in the meantime", function(done) {
+
+      var cnt = 0;
+      var step = function() {
+        spawn(zmq.Context.getMaxSockets() - 100, function(err, clients) {
+          assert.ok(!err);
+          clients.forEach(function(client) {
+            client.stop();
+            cnt++;
+          });
+
+          setImmediate(next);
+        });
+      };
+
+      var next = function() {
+        if (cnt < zmq.Context.getMaxSockets() * 5) {
+          setTimeout(step, 30);
+        } else {
+          done();
+        }
+      };
+      next();
+    });
   });
 });
