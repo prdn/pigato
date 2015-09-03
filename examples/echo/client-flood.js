e51ab9d2e64caa61710f574a6583aae283f2334d
Paolo Ardoino <paolo.ardoino@gmail.com>
2015-09-03 10:46:49 +0100

    Broker code refactoring; switching to use an improved performance queue implementation

--- examples/echo/client-flood.js
+++ examples/echo/client-flood.js
@@ -7,13 +7,17 @@ var client = new Client(conf.broker.host);
 client.start();
 
 client.on('error', function(e) {
+  if (e === 'ERR_REQ_INVALID') {
+    inv++;
+  }
   console.log('ERROR', e);
 });
 
 var d1 = new Date();
-var reqs = 50000;
+var reqs = 100000;
 
 var rcnt = 0;
+var inv = 0;
 
 for (var i = 0; i < reqs; i++) {
   client.request(
@@ -22,7 +26,7 @@ for (var i = 0; i < reqs; i++) {
     function(err, data) {
       rcnt++;
       if (rcnt === reqs) {
-        console.log(reqs + ' requests/replies processed (' + ((new Date()).getTime() - d1.getTime()) + ' milliseconds)');
+        console.log(reqs + ' requests/replies processed (' + ((new Date()).getTime() - d1.getTime()) + ' milliseconds)', 'inv=' + inv);
         process.exit(0);
       }
     }, { timeout: 10000 }
